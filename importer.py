84f9a36c070aefaf21b432db134ed8198f8bf3b6
David Patiashvili <stratagem.david@gmail.com>
2022-07-12 09:09:07 +0200

    Add auto-check link when hover

--- importer.py
+++ importer.py
@@ -15,16 +15,14 @@ with open('data.csv', 'r') as file_csv:
         cursor.execute("SELECT id, like_count FROM uptobox_link WHERE enabled = true AND token = %s", (line['token'],))
         result = cursor.fetchone()
 
-        if result is None:
-            title_vector = line['title'].replace('.', ' ').replace('-', ' ').replace('_', ' ')
-            cursor.execute(
-                'INSERT INTO uptobox_link(date, token, title, size, vector) '
-                'VALUES(%s, %s, %s, %s, to_tsvector(\'french\', %s))',
-                (datetime.now(), line['token'].strip(), line['title'].strip(), int(line['size']) / 1000, title_vector,)
-            )
+        print(f"{line['token']} : {line['title']}")
+
+        if result is not None:
             continue
 
-        like_count = result['like_count'] + 2
-        cursor.execute('UPDATE uptobox_link SET like_count = %s WHERE id = %s', (like_count, result['id'],))
-
-    print(line['token'])
+        title_vector = line['title'].replace('.', ' ').replace('-', ' ').replace('_', ' ')
+        cursor.execute(
+            'INSERT INTO uptobox_link(date, token, title, size, vector) '
+            'VALUES(%s, %s, %s, %s, to_tsvector(\'french\', %s))',
+            (datetime.now(), line['token'].strip(), line['title'].strip(), int(line['size']) / 1000, title_vector,)
+        )
